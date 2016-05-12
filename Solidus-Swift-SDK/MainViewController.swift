8e117dc71e6cb00490a9dc5d37c2b79f2f6811a2
oguzman <a.omargc@gmail.com>
2016-05-12 11:07:39 -0500

    Orders list added to MainViewController

--- Solidus-Swift-SDK/MainViewController.swift
+++ Solidus-Swift-SDK/MainViewController.swift
@@ -8,15 +8,46 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tblOrders: UITableView?
+    var arrOrders = []
+    let cellId = "orderCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tblOrders?.dataSource = self
+        tblOrders?.delegate = self
+        tblOrders?.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        self.loadData()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func loadData() {
+        Orders.List { (success, result, error) in
+            self.arrOrders = result.objectForKey("orders") as! NSArray
+            self.tblOrders?.reloadData()
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrOrders.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let dictItem = arrOrders[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath)
+        cell.textLabel?.text = dictItem.objectForKey("number") as? String
+        
+        return cell
     }
 }
